be378fa79601e60d3c91d591261600b4af5598f6
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-03-05 15:52:35 +0530

    feat: connection string

---
+++ inventory_db_script.sql
@@ -0,0 +1,52 @@
+create database InventoryMgt
+
+use InventoryMgt
+
+create table Category(
+Id int primary key identity,
+CreateDate  datetime not null,
+UpdateDate  datetime not null,
+IsDeleted bit,
+CategoryName nvarchar(50) not null,
+CategoryId int null
+)
+
+create table Product(
+Id int primary key identity,
+CreateDate  datetime not null,
+UpdateDate  datetime not null,
+IsDeleted bit,
+ProductName nvarchar(50) not null,
+CategoryId int not null references Category(Id),
+Price decimal not null
+)
+
+create table Stock(
+Id int primary key identity,
+CreateDate  datetime not null,
+UpdateDate  datetime not null,
+IsDeleted bit,
+ProductId int references Product(Id) unique, 
+Quantity float not null
+)
+
+create table Purchase(
+Id int primary key identity,
+CreateDate  datetime not null,
+UpdateDate  datetime not null,
+IsDeleted bit,
+ProductId int references Product(Id), 
+PurchaseDate datetime not null,
+Quantity float not null,
+[Description] nvarchar(100)
+)
+
+create table Sale(
+Id int primary key identity,
+CreateDate  datetime not null,
+UpdateDate  datetime not null,
+IsDeleted bit,
+ProductId int not null references Product(Id),
+SellingDate datetime not null,Quantity float, 
+[Description] nvarchar(100) not null
+)
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Inventory Mgt
+
+Basic Inventory Management System backend with aspnet core web api.
+
+## TechStack
+
+- Asp.net core web api (8.0)
+- Microsoft SQL Server
+- Dapper (ORM)
+
+## Where do I find database script
+
+Database script is available in inventory_db_script.sql file, which is located in root directory.
+
+## How to run
+
+- Clone this project
+- Modify connection string. Replace server= '{my_server_name}' to '{your_server_name}'.
+- Find the database script and run it.
+- You can run your project now
+
+🙂 Github star is appreciate.
