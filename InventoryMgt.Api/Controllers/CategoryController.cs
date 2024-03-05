22dbe25f2f2e393b7b9070afe5b55b12024e2bf2
Ravindra Devrani <ravindra.devrani003@gmail.com>
2024-03-05 19:55:57 +0530

    feat: category controller remaining methods

--- InventoryMgt.Api/Controllers/CategoryController.cs
+++ InventoryMgt.Api/Controllers/CategoryController.cs
@@ -19,4 +19,32 @@ public class CategoryController : ControllerBase
         var createdCategory = await _categoryRepository.AddCategory(category);
         return CreatedAtAction(nameof(CreateCategory), createdCategory);
     }
+
+    [HttpPut("{Id}")]
+    public async Task<IActionResult> UpdateCategory(int Id, [FromBody] Category category)
+    {
+        await _categoryRepository.UpdateCategory(category);
+        return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetCategories()
+    {
+        var contegories = await _categoryRepository.GetCategories();
+        return Ok(contegories);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetCategory(int id)
+    {
+        var contegory = await _categoryRepository.GetCategory(id);
+        return Ok(contegory);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCategory(int id)
+    {
+        await _categoryRepository.DeleteCategory(id);
+        return NoContent();
+    }
 }
